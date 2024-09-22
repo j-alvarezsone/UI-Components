cda9cdf615946ccffed0030d51ae358140b3ea4b
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2024-09-21 20:52:38 -0500

    chore: added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'roboto-regular': ['Roboto Regular'],
+      'roboto-medium': ['Roboto Medium'],
+      'roboto-bold': ['Roboto Bold'], // Corrected key
+      'geomanist-book': ['Geomanist Book'],
+      'geomanist-regular': ['Geomanist Regular'],
+    },
+  },
+  plugins: [],
+};
