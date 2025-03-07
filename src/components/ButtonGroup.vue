8e8372934e09d9e41457f7bc98dd6f596c227554
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-03-07 14:04:34 +0100

    feat added ButtonGroup component

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    border?: boolean
+  }>(),
+  {
+    border: false,
+  },
+)
+</script>
+
+<template>
+  <span class="button-group" :class="{ 'border border-gray-300 rounded-lg': border }">
+    <slot />
+  </span>
+</template>
+
+<style scoped>
+.button-group :deep(.grouped-button:first-child:not(:only-child)) {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+.button-group :deep(.grouped-button:last-child:not(:only-child)) {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.button-group :deep(.grouped-button:not(:first-child):not(:last-child)) {
+  border-radius: 0;
+}
+
+.button-group :deep(.grouped-button:not(:last-child)) {
+  border-right: 1px solid #d0d5dd;
+}
+
+.button-group :deep(.grouped-button:focus) {
+  position: relative;
+  z-index: 1;
+}
+</style>
